0438a00f7dc5fc6febc4b54af7cbb49fb85565a9
Sohamkulkarni2 <93768445+Sohamkulkarni2@users.noreply.github.com>
2021-11-08 20:25:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function playmusic1(){
+    document.getElementById("audio1").play();
+};
+function playmusic2(){
+    document.getElementById("audio2").play();
+};
+function playmusic3(){
+    document.getElementById("audio3").play();
+};
+function playmusic4(){
+    document.getElementById("audio4").play();
+};
+function playmusic5(){
+    document.getElementById("audio5").play();
+};
